294ece5db596ecb852d601f3c06fbdf4752e83ce
sssdark302 <158068173+sssdark302@users.noreply.github.com>
2024-11-11 10:12:35 +0100

    UML

---
+++ src/main/java/com/example/jurassicpark/JurassicparkApplication.java
@@ -0,0 +1,13 @@
+package com.example.jurassicpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurassicparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JurassicparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jurassicpark/JurassicparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jurassicpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JurassicparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
