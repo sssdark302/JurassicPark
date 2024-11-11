fd5432a4db8264fa3117f9f7dd567544a1c2e56b
sssdark302 <158068173+sssdark302@users.noreply.github.com>
2024-11-12 00:15:54 +0100

    feat/ciclo de vida base completado

---
+++ src/main/java/com/example/jurassicpark/CiclodeVida/CiclodeVida.java
@@ -0,0 +1,9 @@
+package com.example.jurassicpark.CiclodeVida;
+
+public interface CiclodeVida {
+    void nacer();
+    void crecer();
+    void reproducirse();
+    void adulto();
+    void morir();
+}
